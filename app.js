4ca0eaba2d500ca560c135230913951e69962349
Daniel <danielwesleyolson@comcast.net>
2019-02-07 19:50:15 -0800

    working problem 4

--- app.js
+++ app.js
@@ -84,14 +84,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
-
 function sumArray(sumArr) { //eslint-disable-line
+  var total = sum(sum(sumArr[0], sumArr[1]), sumArr[2]);
+  console.log('results: ' + total);
 
+
+
+
+  return total;
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
